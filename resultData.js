91dd84d36b6b84128e6aa06426be3d5891ae90d5
developer-mustafa <mustafarahman792@gmail.com>
2023-06-22 22:12:27 +0600

    added initial file

---
+++ resultData.js
@@ -0,0 +1,58 @@
+var resultData = {
+  students: [
+    {
+      name: "Mustafa Rahman",
+      image:
+        "https://mustafa-portfolio1.netlify.app/static/media/black.0951097db445f3deef3f.jpg",
+      assignments: [
+        { name: "Assignment 1", marks: 50 },
+        { name: "Assignment 2", marks: 45 },
+        { name: "Assignment 3", marks: 45 },
+        // Add more assignment data here
+      ],
+      dailyTasks: [
+        { name: "Daily Task 1", marks: 15 },
+        { name: "Daily Task 2", marks: 18 },
+        { name: "Daily Task 3", marks: 20 },
+        // Add more daily task data here
+      ],
+    },
+    {
+      name: "Sahabuddin",
+      image:
+        "https://mustafa-portfolio1.netlify.app/static/media/black.0951097db445f3deef3f.jpg",
+      assignments: [
+        { name: "Assignment 1", marks: 60 },
+        { name: "Assignment 2", marks: 60 },
+        { name: "Assignment 3", marks: 60 },
+        // Add more assignment data here
+      ],
+      dailyTasks: [
+        { name: "Daily Task 1", marks: 20 },
+        { name: "Daily Task 2", marks: 20 },
+        { name: "Daily Task 3", marks: 20 },
+        // Add more daily task data here
+      ],
+    },
+    {
+      name: "Sohrab Hossen",
+      image:
+        "https://mustafa-portfolio1.netlify.app/static/media/black.0951097db445f3deef3f.jpg",
+      assignments: [
+        { name: "Assignment 1", marks: 50 },
+        { name: "Assignment 2", marks: 60 },
+        { name: "Assignment 3", marks: 60 },
+        { name: "Assignment 4", marks: 58 },
+        // Add more assignment data here
+      ],
+      dailyTasks: [
+        { name: "Daily Task 1", marks: 10 },
+        { name: "Daily Task 2", marks: 20 },
+        { name: "Daily Task 3", marks: 20 },
+        { name: "Daily Task 4", marks: 10 },
+        // Add more daily task data here
+      ],
+    },
+    // Add more student data here
+  ],
+};
