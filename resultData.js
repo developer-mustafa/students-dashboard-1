5dff97c7c24c4787394154307d5230bd95b142b9
developer-mustafa <mustafarahman792@gmail.com>
2023-06-23 00:07:41 +0600

    added students

--- resultData.js
+++ resultData.js
@@ -1,58 +1,89 @@
 var resultData = {
   students: [
     {
-      name: "Mustafa Rahman",
-      image:
-        "https://mustafa-portfolio1.netlify.app/static/media/black.0951097db445f3deef3f.jpg",
-      assignments: [
-        { name: "Assignment 1", marks: 50 },
-        { name: "Assignment 2", marks: 45 },
-        { name: "Assignment 3", marks: 45 },
-        // Add more assignment data here
-      ],
-      dailyTasks: [
-        { name: "Daily Task 1", marks: 15 },
-        { name: "Daily Task 2", marks: 18 },
-        { name: "Daily Task 3", marks: 20 },
-        // Add more daily task data here
-      ],
-    },
-    {
-      name: "Sahabuddin",
-      image:
-        "https://mustafa-portfolio1.netlify.app/static/media/black.0951097db445f3deef3f.jpg",
+      name: "মোঃ সোহরাব হোসেন",
+      image: "./img-students/sohrab.jpg",
       assignments: [
         { name: "Assignment 1", marks: 60 },
-        { name: "Assignment 2", marks: 60 },
-        { name: "Assignment 3", marks: 60 },
+
         // Add more assignment data here
       ],
       dailyTasks: [
         { name: "Daily Task 1", marks: 20 },
-        { name: "Daily Task 2", marks: 20 },
-        { name: "Daily Task 3", marks: 20 },
+
         // Add more daily task data here
       ],
     },
     {
-      name: "Sohrab Hossen",
-      image:
-        "https://mustafa-portfolio1.netlify.app/static/media/black.0951097db445f3deef3f.jpg",
+      name: "মোঃ সাহাবুদ্দিন",
+      image: "./img-students/sahabuddin.jpg",
       assignments: [
-        { name: "Assignment 1", marks: 50 },
-        { name: "Assignment 2", marks: 60 },
-        { name: "Assignment 3", marks: 60 },
-        { name: "Assignment 4", marks: 58 },
+        { name: "Assignment 1", marks: 60 },
+
         // Add more assignment data here
       ],
       dailyTasks: [
-        { name: "Daily Task 1", marks: 10 },
-        { name: "Daily Task 2", marks: 20 },
-        { name: "Daily Task 3", marks: 20 },
-        { name: "Daily Task 4", marks: 10 },
+        { name: "Daily Task 1", marks: 20 },
         // Add more daily task data here
       ],
     },
-    // Add more student data here
+    {
+      name: "মোঃ রোহান উদ্দিন",
+      image:
+        "https://learnwithmustafa.netlify.app/static/media/mustafa2.35a7cef0833484f6fa4b.jpeg",
+      assignments: [
+        { name: "Assignment 1", marks: 60 },
+
+        // Add more assignment data here
+      ],
+      dailyTasks: [
+        { name: "Daily Task 1", marks: 20 },
+
+        // Add more daily task data here
+      ],
+    },
+    {
+      name: "জান্নাতুল আক্তার প্রমি",
+      image: "./img-students/jannat.png",
+      assignments: [
+        { name: "Assignment 1", marks: 60 },
+
+        // Add more assignment data here
+      ],
+      dailyTasks: [
+        { name: "Daily Task 1", marks: 20 },
+
+        // Add more daily task data here
+      ],
+    },
+
+    {
+      name: "মোঃ আরাফাত হোসাইন সায়েম",
+      image: "./img-students/sayem.jpg",
+      assignments: [
+        { name: "Assignment 1", marks: 60 },
+
+        // Add more assignment data here
+      ],
+      dailyTasks: [
+        { name: "Daily Task 1", marks: 20 },
+
+        // Add more daily task data here
+      ],
+    },
+    {
+      name: "মোঃ সোলায়মান",
+      image: "./img-students/solayman.png",
+      assignments: [
+        { name: "Assignment 1", marks: 60 },
+
+        // Add more assignment data here
+      ],
+      dailyTasks: [
+        { name: "Daily Task 1", marks: 20 },
+
+        // Add more daily task data here
+      ],
+    },
   ],
 };
