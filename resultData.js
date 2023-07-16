285e258d11dcd7681eb0bc3c226eb7b01a317081
developer-mustafa <mustafarahman792@gmail.com>
2023-07-16 18:58:09 +0600

    added colorful mode

--- resultData.js
+++ resultData.js
@@ -2,92 +2,115 @@ var resultData = {
   students: [
     {
       name: "মোঃ সোহরাব হোসেন",
-      projectName: "Text formatting web page",
+
       image: "./img-students/sohrab.jpg",
       assignments: [
-        { name: "Assignment 1", marks: 60 },
+        { name: " Text-formatting ", marks: 60 },
+        { name: "Table with image ", marks: 60 },
+        { name: "Social Media Card (Profile-Card) ", marks: 60 },
 
         // Add more assignment data here
       ],
       dailyTasks: [
-        { name: "Daily Task 1", marks: 20 },
+        { name: "Task Week 1", marks: 20 },
+        { name: "Task Week 2", marks: 20 },
+        { name: "Task Week 3", marks: 20 },
 
-        // Add more daily task data here
+        // Add more Task Week data here
       ],
     },
     {
-      name: "মোঃ সাহাবুদ্দিন",
-      projectName:  "Text formatting web page",
+      name: "মোঃ সাহাবউদ্দিন",
       image: "./img-students/sahabuddin.jpg",
       assignments: [
-        { name: "Assignment 1", marks: 60 },
+        { name: "Text-formatting", marks: 60 },
+        { name: "Table with image ", marks: 60 },
+        { name: "Social Media Card (Profile-Card) ", marks: 60 },
 
         // Add more assignment data here
       ],
       dailyTasks: [
-        { name: "Daily Task 1", marks: 20 },
-        // Add more daily task data here
+        { name: "Task Week 1", marks: 20 },
+        { name: "Task Week 2", marks: 20 },
+        { name: "Task Week 3", marks: 20 },
+        // Add more Task Week data here
       ],
     },
     {
       name: "মোঃ রোহান উদ্দিন",
-      projectName:  "Text formatting web page",
+
       image: "./img-students/rohan.jpg",
       assignments: [
-        { name: "Assignment 1", marks: 60 },
+        { name: " Text-formatting ", marks: 60 },
+        { name: "Table with image ", marks: 60 },
+        { name: "Social Media Card (Profile-Card) ", marks: 0 },
 
         // Add more assignment data here
       ],
       dailyTasks: [
-        { name: "Daily Task 1", marks: 20 },
+        { name: "Task Week 1", marks: 20 },
+        { name: "Task Week 2", marks: 20 },
+        { name: "Task Week 3", marks: 0 },
 
-        // Add more daily task data here
+        // Add more Task Week data here
       ],
     },
     {
       name: "মিফতাহুল জান্নাত প্রমি",
-      projectName:  "Text formatting web page",
+
       image: "./img-students/jannat.png",
       assignments: [
-        { name: "Assignment 1", marks: 60 },
+        { name: " Text-formatting ", marks: 60 },
+        { name: "Table with image ", marks: 60 },
+        { name: "Social Media Card (Profile-Card) ", marks: 60 },
 
         // Add more assignment data here
       ],
       dailyTasks: [
-        { name: "Daily Task 1", marks: 20 },
+        { name: "Task Week 1", marks: 20 },
+        { name: "Task Week 2", marks: 20 },
+        { name: "Task Week 3", marks: 20 },
 
-        // Add more daily task data here
+        // Add more Task Week data here
       ],
     },
 
     {
       name: "মোঃ আরাফাত হোসাইন সায়েম",
-      projectName:  "Text formatting web page",
+
       image: "./img-students/sayem.jpg",
       assignments: [
-        { name: "Assignment 1", marks: 60 },
+        { name: " Text-formatting ", marks: 60 },
+        { name: "Table with image ", marks: 60 },
+        { name: "Social Media Card (Profile-Card) ", marks: 60 },
 
         // Add more assignment data here
       ],
       dailyTasks: [
-        { name: "Daily Task 1", marks: 20 },
+        { name: "Task Week 1", marks: 20 },
+        { name: "Task Week 2", marks: 20 },
+        { name: "Task Week 3", marks: 20 },
 
-        // Add more daily task data here
+        // Add more Task Week data here
       ],
     },
     {
       name: "মোঃ সোলায়মান",
-      projectName:  "Text formatting web page",
+
       image: "./img-students/solayman.png",
       assignments: [
-        { name: "Assignment 1", marks: 60 },
+        { name: " Text-formatting ", marks: 0 },
+        { name: "Table with image ", marks: 0 },
+        { name: "Social Media Card (Profile-Card) ", marks: 0 },
 
         // Add more assignment data here
       ],
       dailyTasks: [
-        { name: "Daily Task 1", marks: 20 },
+        { name: "Task Week 1", marks: 0 },
+        { name: "Task Week 2", marks: 0 },
+        { name: "Task Week 3", marks: 0 },
 
-        // Add more daily task data here
+        // Add more Task Week data here
       ],
     },
   ],
